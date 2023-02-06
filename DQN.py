d452c120744c1ee855049584aa3d10c666c8e445
Callum Alexander <callumalexander.personal@gmail.com>
2023-02-06 14:50:41 +0000

    Implemented the Deep Q Learning Network class that is a subclass of torch.nn.module

--- DQN.py
+++ DQN.py
@@ -7,13 +7,25 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-class DQN:
+class DQN(nn.module):
+    """
+    Our aim will be to train a policy that tries to maximize the discounted,
+    cumulative reward
+    """
 
-    def __init__(self):
-        """
-        Constructor
-        """
-        pass
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        # Create a 3 layer netword with 128 hidden fc units per layer
+        self.layer1 = nn.Linear(n_observations, 128)  # (in_f, out_f)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        # The feed forward method
+        # Applying Rec Linear to each layer
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)  # Returns the final layer
 
 
 Transition = namedtuple('Transition',
