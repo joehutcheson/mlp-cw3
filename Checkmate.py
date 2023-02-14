a5cec6e2de50cd959b5fd18eb836f31cf0701ede
Callum Alexander <callumalexander.personal@gmail.com>
2023-02-14 14:48:07 +0000

    Initialised OperationCheckmate file

---
+++ Checkmate.py
@@ -0,0 +1,15 @@
+
+class Checkmate:
+    """
+    Checkmate - a python class used for converting Stockfish python API notation to
+    Pettingzoo chess environment action space, and vice versa.
+    """
+
+    def __init__(self):
+        pass
+
+    def stockfish2pettingzoo(self):
+        pass
+
+    def pettingzoo2stockfish(self):
+        pass
