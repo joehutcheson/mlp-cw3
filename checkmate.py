4c99f9d16e24ebab7af3dbcabe8dc7b27b5fdb51
Callum Alexander <callumalexander.personal@gmail.com>
2023-02-28 07:24:06 +0000

    Stockfish to PettingZoo works

--- checkmate.py
+++ checkmate.py
@@ -1,8 +1,40 @@
-import numpy as np
 from pettingzoo.classic.chess import chess_utils
 
 
-import stockfish
+def stockfish2pettingzoo(move):
+    """
+    Stockfish comes in the regex format ^\d[a-z]\d[a-z]$ - string of
+    length 4, 1st & 3rd chars are lowercase letters, 2nd & 4th chars
+    are numbers.
+
+    Process:
+        - Stockfish looks at the current state and produces an ideal move
+        in the format ^[a-z]\d[a-z]\d$ (UCI). Stockfish already has built-in
+        checking for legal moves, therefore, the method doesn't need to
+        check.
+
+        - This method converts the UCI into an action value.
+
+        - That action value is played on the Stockfish agent's behalf.
+
+    :param move UCI format string of this ^\d[a-z]\d[a-z]$
+
+    :return: an integer representing the index of the action to be played.
+    """
+
+    # This method makes a move-mapping and inserts it into the two
+    # dictionaries.
+    assert isinstance(move, str), f"Move is not a string, got {move} instead"
+    chess_utils.make_move_mapping(uci_move=move)
+    print(chess_utils.actions_to_moves)
+    action = None
+    for act, uci in chess_utils.actions_to_moves.items():
+        if uci == move:
+            action = act
+
+    assert isinstance(action, int), f"action should be an integer, got {action} instead"
+
+    return action
 
 
 class Checkmate:
@@ -10,26 +42,6 @@ class Checkmate:
     Checkmate - a python class used for converting Stockfish python API
     notation to Pettingzoo chess environment action space, and vice versa.
     """
-
-    def __init__(self):
-        pass
-
-    def stockfish2pettingzoo(self, move):
-        """
-        Stockfish comes in the regex format ^\d[a-z]\d[a-z]$ - string of
-        length 4, 1st & 3rd chars are numbers, 2nd & 4th chars are lowercase
-        letters  TODO - Fix this shit it's letter number, not number letter
-
-        :param move - UCI format string of this ^\d[a-z]\d[a-z]$
-
-        :return: a standard basis vector of size 4672 representing the output
-        action to be fed to the environment (8, 8, 73)
-        """
-
-        action = chess_utils.actions_to_moves[move]
-        return action
-
-
 
     def pettingzoo2stockfish(self):
         pass
