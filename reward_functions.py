cd64261bfb8c220a8d496dfddec635a34cf4f0f4
Callum Alexander <callumalexander.personal@gmail.com>
2023-03-05 15:11:16 +0000

    Further init commit for reward functions

--- reward_functions.py
+++ reward_functions.py
@@ -1,12 +1,52 @@
+__author__ = "Callum Alexander"
+__email__ = "s1931801@ed.ac.uk"
+
+import chess as pychess
+from pettingzoo.utils import OrderEnforcingWrapper
 
 
 class RewardFunction:
 
     def __init__(self, env_, agent_):
-
         self.env = env_
         self.agent = agent_
 
     def get_reward(self):
         raise NotImplementedError()
 
+
+def piece_capture_reward(env: OrderEnforcingWrapper) -> int:
+    """
+    A positive reward signal for capturing opponent's pieces.
+    agent makes move
+    checks for reward
+        check that the previous move was legal
+        check available pieces on the board,
+            if material change  how do you check material change?
+                previous move resulted in a taking of material
+            reward
+    :param env: OrderEnforcingWrapper instance representing the chess
+                environment
+    :return: int representing the reward at the given state
+    """
+    pass
+
+
+def material_advantage_reward(env: OrderEnforcingWrapper) -> int:
+    """
+     A positive reward signal for having a material advantage over the opponent.
+    :param env: OrderEnforcingWrapper instance representing the chess
+                environment
+    :return: int representing the reward at the given state
+    """
+    pass
+
+
+def mobility_reward(env):
+    """
+    A positive reward signal for having a greater number of available moves.
+    :param env: OrderEnforcingWrapper instance representing the chess
+                environment
+    :return: int representing the reward at the given state
+    """
+    pass
