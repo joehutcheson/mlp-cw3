a4b5da2a6d751166491535b8c498ad2d46db2131
joe <63621767+joehutcheson@users.noreply.github.com>
2023-03-19 14:56:05 +0000

    Mobility reward run files

---
+++ mobility.py
@@ -0,0 +1,16 @@
+import testing_torch_model
+from reward_functions import *
+from pettingzoo.classic import chess_v5 as chess
+
+env = chess.env()
+env.reset()
+
+m = '/opt/homebrew/Cellar/stockfish/15.1/bin/stockfish'
+d = '/home/s1951999/stockfish_15.1_linux_x64/stockfish-ubuntu-20.04-x86-64'
+
+model = testing_torch_model.Model(env, reward_function=mobility_reward,
+                                  stockfish_path=d,
+                                  stockfish_difficulty=500)
+
+model.train(1000)
+model.save_model('player_0', 'models/', 'mobility')
