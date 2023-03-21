aa1b83f7734e7af6f6d59321eb4b97001222443d
joe <63621767+joehutcheson@users.noreply.github.com>
2023-03-21 11:31:47 +0000

    Testing

--- basic.py
+++ basic.py
@@ -9,8 +9,7 @@ m = '/opt/homebrew/Cellar/stockfish/15.1/bin/stockfish'
 d = '/home/s1951999/stockfish_15.1_linux_x64/stockfish-ubuntu-20.04-x86-64'
 
 model = testing_torch_model.Model(env, reward_function=material_advantage_reward,
-                                  stockfish_path=d,
-                                  stockfish_difficulty=500)
+                                  reward_function_2=material_advantage_reward)
 
 model.load_model('player_0', 'models/', 'material_advantage')
 model.load_model('player_1', 'models/', 'material_advantage')
